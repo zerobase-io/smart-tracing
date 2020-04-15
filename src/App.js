f76cf75195b3a10bcd0cc698b6feb523940c7d80
Veselin Dimitrov <veselin@toptal.com>
2020-04-15 13:24:28 +0300

    Port the s/:id route

--- src/App.js
+++ src/App.js
@@ -105,6 +105,59 @@ const App = () => {
     }
   });
 
+  const renderSdvidRoute = (props) => {
+    const sdvid = props.match.params.dvid;
+    const dvid =
+      localStorage.getItem('dvid') === 'undefined'
+        ? undefined
+        : localStorage.getItem('dvid');
+    window.history.replaceState(null, null, '/');
+
+    if (sdvid && window.innerWidth < 750) {
+      if (dvid) {
+        console.log('Has registered ID on scan');
+        $('#modal-scan-notice').modal('show');
+        // Please note this callback pattern to pass data down the "chain"
+        // Unfortunately cannot use Async Await due to coverage issues.
+        controller.fetchIP({ sdvid }, (data) => {
+          controller.fingerprint(data, (fingerprintData) => {
+            controller.scan(fingerprintData, () => {
+              return <HomePage />;
+            });
+          });
+        });
+      } else {
+        console.log('Does not have registed ID on scan');
+        // patch, since if user hits cancel its never routed to init
+        if (window.innerWidth < 750) {
+          $('#desktop-notice').addClass('d-none');
+        }
+        $('#mobile-functions').addClass('d-none');
+        $('#registration-notice').removeClass('d-none');
+        $('#modal-register-notice').modal('show');
+        $('body').on('click', '#register-notice-agree', () => {
+          $('#register-notice-agree').addClass('btn-loading');
+          $('#modal-scan-notice').modal('show');
+          controller.fetchIP({ sdvid }, (data) => {
+            console.log('IP -> fingerprint:', data);
+            controller.fingerprint(data, (fingerprintData) => {
+              console.log('fingerprint -> create :', fingerprintData);
+              controller.create(fingerprintData, (createData) => {
+                console.log('create -> scan: ', createData);
+                controller.scan(createData, () => {
+                  return <HomePage />;
+                });
+              });
+            });
+          });
+        });
+      }
+    } else {
+      console.log('Invalid Access Point');
+      return <HomePage />;
+    }
+  };
+
   return (
     <>
       <div id="app">
@@ -164,6 +217,7 @@ const App = () => {
               <Route path="/healthcare/register">
                 <HealthCareRegisterPage />
               </Route>
+              <Route path="/s/:dvid" render={renderSdvidRoute} />
               <Route exact path="/">
                 <HomePage />
               </Route>
