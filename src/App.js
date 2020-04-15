4640fa2fda75f7e13a8826ae738f0a52e274d6ca
Veselin Dimitrov <veselin@toptal.com>
2020-04-15 08:03:02 +0300

    Integrate some of the components in the app

--- src/App.js
+++ src/App.js
@@ -1,64 +1,45 @@
 import React from 'react';
-import styled from 'styled-components';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import view from './view.pug';
-
-// An example of how to use styled-components
-// This is just a normal CSS, but defined through styled-components
-// for a simple img tag
-const Logo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-`;
-
-// This is an example of how you override an existing component with custom CSS
-const CustomLink = styled(Link)`
-  color: #61dafb;
-`;
-
-const Home = () => {
-  return (
-    <div className="App-container">
-      <Logo src="/assets/img/logo_icon_200.png" alt="logo" />
-      <h1>Zerobase</h1>
-      <p>Privacy-First Contact Tracing for Communities</p>
-      <CustomLink to="/scan" rel="noopener noreferrer">
-        Scan
-      </CustomLink>
-    </div>
-  );
-};
-
-class View extends React.Component {
-  render() {
-    return view.call(this, {});
-  }
-}
-
-const Scan = () => {
-  return (
-    <div>
-      <p>This is the page for scanning QR codes</p>
-      <View />
-    </div>
-  );
-};
+import Primary from './components/navigation/Primary';
+import Secondary from './components/navigation/Secondary';
+import Footer from './components/navigation/Footer';
+import Register from './components/modal/Register';
+import ScanStatus from './components/modal/ScanStatus';
+import Feedback from './components/modal/Feedback';
+import RegisterNotifications from './components/modal/RegisterNotifications';
+import PrivacyPolicy from './components/modal/PrivacyPolicy';
+import Home from './components/pages/Home';
+import Privacy from './components/pages/Privacy';
 
 const App = () => {
   return (
-    <div className="App" id="app">
-      <Router>
-        <Switch>
-          <Route path="/scan">
-            <Scan />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <>
+      <div className="App" id="app">
+        <div className="page">
+          <Primary />
+          <Secondary />
+          <Router>
+            <Switch>
+              <Route path="/privacy">
+                <Privacy />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+        <Footer />
+      </div>
+      <Register />
+      {/* <RegisterBusiness /> */}
+      {/* <RegisterHealthcare /> */}
+      <ScanStatus />
+      <Feedback />
+      <RegisterNotifications />
+      <PrivacyPolicy />
+    </>
   );
 };
 
