4ff2c67aef301350a1a692135be29653880907c7
Veselin Dimitrov <veselin@toptal.com>
2020-04-24 17:16:35 +0300

    Add colors constants

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  primary: '#293742',
+  secondary: '#64C7E1',
+  light: '#FFFFFF',
+  dark: '#354052',
+  gray: '#A6A9AC',
+  lighterGray: '#D3D4D5',
+  green: '#79B977',
+  lightGreen: '#E3F0CF',
+  yellow: '#FDDF9B',
+  lighterYellow: '#FEEFCD',
+  blue: '#67C4E0',
+  red: '#DF2021',
+};
+
+export default colors;
