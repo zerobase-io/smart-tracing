a1d42a7a8c2facfa4bbdc58a46f0ad999ce528e2
Veselin Dimitrov <veselin@toptal.com>
2020-04-24 18:10:46 +0300

    Add fontSizes constants

---
+++ src/styles/fontSizes.js
@@ -0,0 +1,15 @@
+// These are the font sizes in the app
+// Any components should use font sizes which are defined here
+// There shouldn't be cases where we are supposed to define custom sizes across the app
+
+export const fontSizes = {
+  banner: '60px',
+  largeTitle: '30px',
+  title: '24px',
+  large: '18px',
+  primary: '16px',
+  secondary: '14px',
+  small: '12px',
+};
+
+export default fontSizes;
