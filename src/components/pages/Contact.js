ff7da201c1d3977eee87a00a0c2888412f9d3610
Lukas Peterson <lpeterson@standardimaging.com>
2020-04-25 11:02:02 -0500

    Removed onmousewheel from Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,7 +7,7 @@ class Contact extends React.Component {
         <div className="container" style={{padding: 0}}>
           <iframe className="airtable-embed airtable-dynamic-height"
                   src="https://airtable.com/embed/shrnYjRudkIBlXzr9?backgroundColor=yellow"
-                  frameBorder="0" onmousewheel="" width="100%" height="1149"
+                  frameBorder="0" width="100%" height="1149"
                   style={{background: 'transparent',
                     border: '1px solid #ccc'
                   }}>
