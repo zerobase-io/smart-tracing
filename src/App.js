91424a0ad82733ce12e89de2e3145ced01f21f1b
justin-reeves <kjryan.reeves@gmail.com>
2021-01-12 11:48:09 -0800

    Remove pages that are currently using backend

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import controller from './lib/controller';
+/* import controller from './lib/controller'; */
 
 import Primary from './components/navigation/Primary';
 import Secondary from './components/navigation/Secondary';
 import Footer from './components/navigation/Footer';
 
-import RegisterModal from './components/modal/Register';
-import ScanStatusModal from './components/modal/ScanStatus';
+/* import RegisterModal from './components/modal/Register';
+import ScanStatusModal from './components/modal/ScanStatus'; */
 import FeedbackModal from './components/modal/Feedback';
-import RegisterNotificationsModal from './components/modal/RegisterNotifications';
+/* import RegisterNotificationsModal from './components/modal/RegisterNotifications'; */
 import PrivacyPolicyModal from './components/modal/PrivacyPolicy';
 
 import HomePage from './components/pages/Home';
@@ -23,7 +23,7 @@ import IndividualsLandingPage from './components/pages/IndividualsLanding';
 import CommunityPage from './components/pages/Community';
 import BusinessLandingPage from './components/pages/BusinessLanding';
 import TestingsiteLandingPage from './components/pages/TestingsiteLanding';
-import NotificationsPage from './components/pages/Notifications';
+/* import NotificationsPage from './components/pages/Notifications'; */
 import PrivacyPage from './components/pages/Privacy';
 import PrivacyLandingPage from './components/pages/PrivacyLanding';
 import TermsPage from './components/pages/Terms';
@@ -31,31 +31,31 @@ import FeedbackPage from './components/pages/Feedback';
 import ContactPage from './components/pages/Contact';
 import VolunteerLandingPage from './components/pages/VolunteerLanding';
 import OurTeamPage from './components/pages/OurTeam';
-import ScanPage from './components/pages/Scan';
+/* import ScanPage from './components/pages/Scan';
 import BusinessRegisterPage from './components/pages/BusinessRegister';
 import HealthCareRegisterPage from './components/pages/HealthCareRegister';
 import TestPage from './components/pages/Test';
 
-import Scanner from './components/components/Scanner';
+import Scanner from './components/components/Scanner'; */
 
 const App = () => {
     // Modals --------------------------------------------------//
     $('body').on('click', '#privacy-policy', () => {
         $('#modal-privacy-policy').modal('show');
     });
-    $('body').on('click', '#show-registration', () => {
+    /* $('body').on('click', '#show-registration', () => {
         $('#modal-register-notice').modal('show');
-    });
+    }); */
     $('body').on('click', '#show-feedback', () => {
         $('#modal-feedback').modal('show');
     });
-    $('body').on('click', '#show-register-notifications', () => {
+    /* $('body').on('click', '#show-register-notifications', () => {
         $('#modal-register-notifications').modal('show');
-    });
+    }); */
 
     // Forms (Need refactor) --------------------------------------------------//
 
-    $('body').on('click', '#notify-submit', (e) => {
+    /* $('body').on('click', '#notify-submit', (e) => {
         e.preventDefault();
         if ($('#notify-agree').prop('checked') === true) {
             $('#notify-submit').addClass('btn-loading');
@@ -66,7 +66,7 @@ const App = () => {
         } else {
             $('#notify-warning').removeClass('d-none');
         }
-    });
+    }); */
 
     return (
         <>
@@ -91,9 +91,9 @@ const App = () => {
                             <Route path="/testing">
                                 <TestingsiteLandingPage />
                             </Route>
-                            <Route path="/notifications">
+                            {/* <Route path="/notifications">
                                 <NotificationsPage />
-                            </Route>
+                            </Route> */}
                             <Route path="/privacy-policy">
                                 <PrivacyPage />
                             </Route>
@@ -115,7 +115,7 @@ const App = () => {
                             <Route path="/team">
                                 <OurTeamPage />
                             </Route>
-                            <Route path="/scan">
+                            {/* <Route path="/scan">
                                 <ScanPage />
                             </Route>
                             <Route path="/business/register">
@@ -127,7 +127,7 @@ const App = () => {
                             <Route path="/s/:sdvid" component={Scanner} />
                             <Route path="/test">
                                 <TestPage />
-                            </Route>
+                            </Route> */}
                             <Route exact path="/">
                                 <HomePage />
                             </Route>
@@ -137,10 +137,10 @@ const App = () => {
                 </div>
                 <Footer />
             </div>
-            <RegisterModal />
-            <ScanStatusModal />
+            {/* <RegisterModal />
+            <ScanStatusModal /> */}
             <FeedbackModal />
-            <RegisterNotificationsModal />
+            {/* <RegisterNotificationsModal /> */}
             <PrivacyPolicyModal />
         </>
     );
